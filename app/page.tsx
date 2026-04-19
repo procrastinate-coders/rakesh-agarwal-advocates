86817fd9bb6832636f1a7cb5d0314fa4ea5af79e
uddugarg <udivyy.99@gmail.com>
2026-04-19 15:28:01 +0530

    feat: integrate PullQuote component into Hero section and update layout styles

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,6 @@ export default function Home() {
       <PracticeRibbon />
       <PortraitEditorial />
       <AboutSection />
-      <PullQuote />
 
       <Timeline
         section={SECTIONS.education}
--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import ScrollReveal from '@/components/shared/ScrollReveal';
 import { SITE, HERO_INTRO, HERO_ROLES } from '@/lib/constants';
+import PullQuote from './PullQuote';
 
 export default function Hero() {
   return (
@@ -14,6 +15,9 @@ export default function Hero() {
             {SITE.name.split(' ')[1]}, <em>{SITE.suffix}</em>
           </h1>
           <p className="gm-hero__sub">{SITE.subtitle}</p>
+          <ScrollReveal className="gm-hero__intro-text">
+            <p>{HERO_INTRO}</p>
+          </ScrollReveal>
         </div>
         <div className="gm-hero__photo">
           <Image
@@ -27,11 +31,7 @@ export default function Hero() {
           />
         </div>
       </div>
-
-      <ScrollReveal className="gm-hero__intro-text">
-        <p>{HERO_INTRO}</p>
-      </ScrollReveal>
-
+      <PullQuote />
       <ScrollReveal className="gm-hero__roles">
         {HERO_ROLES.map((role) => (
           <div key={`${role.label}-${role.emphasis}`} className="gm-hero__role">
